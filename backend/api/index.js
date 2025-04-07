ac448587ca8c2d34a065abd838d610d25e36da88
daniprieto <dani101val@gmail.com>
2025-04-07 12:31:04 -0400

    update CORS configuration to allow requests from the new backend URL

--- backend/api/index.js
+++ backend/api/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ["http://localhost:5173", "https://your-frontend-url.vercel.app"],
+  origin: ["http://localhost:5173", "https://mini-project-3-backend.vercel.app/"],
   credentials: true
 }));
 
